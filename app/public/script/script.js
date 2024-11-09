43a560201a00a51217803ee2680de0fb39884364
moni.yang <moni.yang@onelab.tw>
2024-11-09 18:00:10 +0800

    feat: deposit & withdraw

--- app/public/script/script.js
+++ app/public/script/script.js
@@ -27,11 +27,30 @@
 }
 
 function prodiverOpen() {
+   const allElements = document.querySelectorAll('[data-open]');
+   allElements.forEach((el) => {
+     if (!el.classList.contains('provider')) {
+       el.setAttribute('data-open', 'false');
+     }
+   });
+
+  // Set `data-open` to 'true' for the `.provider` element
   const provider = document.querySelector('.provider');
-  
-   // 強制設定 data-open 屬性為 'true'，只管開
-   provider.setAttribute('data-open', 'true');
+  if (provider) {
+    provider.setAttribute('data-open', 'true');
+  }
 }
+function homeEvent() {
+  
+  const allElements = document.querySelectorAll('[data-open]');
+  allElements.forEach((el) => {
+    if (!el.classList.contains('provider')) {
+      el.setAttribute('data-open', 'false');
+    }
+  });
+}
+
+
 
 function prodiverclose() {
   
@@ -57,4 +76,14 @@ function promoEvent() {
   
   // 切換 data-open 屬性
   promo.setAttribute('data-open', !isOpen);
+}
+
+function walletEvent() {
+  const wallet = document.querySelector('.wallet');
+  
+  // 檢查目前的 data-open 屬性值
+  const isOpen = wallet.getAttribute('data-open') === 'true';
+  
+  // 切換 data-open 屬性
+  wallet.setAttribute('data-open', !isOpen);
 }
